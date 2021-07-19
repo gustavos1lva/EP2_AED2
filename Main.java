fdeae1921cd95d57544afe1ee0d7d0105c4077ce
Gustavo <gustavosilva1@usp.br>
2021-07-19 19:11:51 -0300

    exclusao inuteis

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Digrafo di = new Digrafo(5);
-        di.insercaoAleatoria(5,0.5,4);
-
-        di.mostraDigrafo();
-    }
-}
