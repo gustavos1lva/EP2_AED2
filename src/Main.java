7ea6e7a111bd05305b07bd8b966132ba2d183a5c
Gustavo <gustavosilva1@usp.br>
2021-07-21 18:29:33 -0300

    main pronto

--- src/Main.java
+++ src/Main.java
@@ -18,32 +18,36 @@ public class Main {
         int tipo = s.nextInt();
 
         switch (tipo) {
-            case 1 -> di.insercaoAleatoria(p, pesoArco);
-            case 2 -> di.insercaoAleatoriaDAG(p, pesoArco);
+            case 1:
+                di.insercaoAleatoria(p, pesoArco);
+                break;
+            case 2:
+                di.insercaoAleatoriaDAG(p, pesoArco);
+                break;
         }
 
         System.out.println("Insira o numero da operação desejada: " + "\n"
         + "1: Dijkstra" + "\n" + "2: DAGMin" + "\n" + "3: BellmanFord");
         int ops = s.nextInt();
         switch (ops) {
-            case 1 -> {
+            case 1:
                 System.out.println("Insira o vertice inicial desejado para o caminho de Dijkstra: ");
                 int inicialDijkstra = s.nextInt();
                 Dijkstra dj = new Dijkstra();
                 dj.Dijkstra(di, inicialDijkstra);
-            }
-            case 2 -> {
+                break;
+            case 2:
                 System.out.println("Insira o vertice inicial desejado para o caminho de DAGMin: ");
                 int inicialDag = s.nextInt();
                 DagMin dag = new DagMin();
                 dag.DagMin(di, inicialDag);
-            }
-            case 3 -> {
+                break;
+            case 3:
                 System.out.println("Insira o vertice inicial desejado para o caminho de BellmanFord: ");
                 int inicialBell = s.nextInt();
                 BellmanFord bell = new BellmanFord();
                 bell.bellman_ford(di, inicialBell);
-            }
+                break;
         }
 
     }
