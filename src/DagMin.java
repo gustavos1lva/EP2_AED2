ef481fdac989104bf98938b375e2165bbecdb4e6
Bruno Daniel <brunodmp98@usp.br>
2021-07-23 22:04:27 -0300

    Apenas o if na linha 21 do DAGMin

--- src/DagMin.java
+++ src/DagMin.java
@@ -18,6 +18,7 @@ public class DagMin {
 
             if (custos[vertice] == infinito) continue;
             for (int k = 0; k < di.vertices[v].size(); k++){
+                if(di.vertices[vertice].size()==0)continue;//fixme mudança feita para arrumar o arrayOutOfBounds que dava quando chamava o metodo
                 p = di.vertices[v = vertice].get(k);
 
                 if (custos[p.verticeDestino] > custos[vertice] + p.peso){
